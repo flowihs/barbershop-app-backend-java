ac4c4e8149f7596bfe5ba74e0c9ddc2fc33b94f1
flowihs <zmulenko500@gmail.com>
2026-09-12 14:38:37 +0300

    fix: provision repository

--- src/main/java/com/github/barbershop/provision/repository/ProvisionRepository.java
+++ src/main/java/com/github/barbershop/provision/repository/ProvisionRepository.java
@@ -1,6 +1,7 @@
 package com.github.barbershop.provision.repository;
 
 import com.github.barbershop.provision.entity.Provision;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProvisionRepository extends JpaRepository<Provision, Long> {
+    @EntityGraph(attributePaths = {"slots", "provisionCategory", "user"})
+    List<Provision> findAll();
+
+    @EntityGraph(attributePaths = {"slots", "provisionCategory", "user"})
+    Optional<Provision> findById(Long id);
+
     Optional<Provision> findByIdAndUserId(Long id, Long userId);
 
     @Query("SELECT p.user.id FROM Provision p WHERE p.id = :provisionId")
